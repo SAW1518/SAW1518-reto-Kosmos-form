f4e9a401cdb53db7c18d98f0c4b672eefbf53deb
SAW1518 <enriquepppsaw@gmail.com>
2022-01-17 13:50:25 -0600

    fix

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -14,7 +14,7 @@ export default function Form ({controlsAdded, setControlsAdded}){
     return (
         <div className='flex  flex-col h-full w-1/2  justify-center items-center bg-[#525558]'>
             <h1 className='text-white' >
-                Didponibles:
+                Agregados:
             </h1>
             <div className="w-full max-w-xs ">
                 <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
